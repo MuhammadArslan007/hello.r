e0d3b76139bc05718924a399e002c2702edbcce0
MuhammadArslan007 <muhammadarslanjattt@gmail.com>
2021-11-28 15:19:23 +0500

    this is my first code with r in data science course of al nafi

--- test.r
+++ test.r
@@ -1,1 +1,23 @@
 
+#vrabile
+#locate memory 
+
+a=6
+b=2
+a+b
+#Data type
+#are 5 data type 
+###vector,matrix,list,arry,data frame 
+#vector 
+vtr1=c(1,2,2,34,4)
+vtr2=c('a','b','c','d','f')
+vtr1
+vtr2
+vtr=c(1,2,2,3,4)
+vtr
+vector(mode="logical", length=5)
+matrix(1:4,nrow=2,ncol=2)
+matrix(1:4,nrow=2,ncol=2,byrow=T,dimname=list(c("a","b"),c("d","f")))
+s=list("a",1,4L,T,names=c("1","2"))
+names(vtr1)=c("a","b","c","d","f")
+vtr1
